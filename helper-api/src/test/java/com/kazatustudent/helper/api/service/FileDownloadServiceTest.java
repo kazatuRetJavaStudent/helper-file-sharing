37d886f40da7e1741919a76e69f8636a4b9a86d6
N <ret2216kazatu@gmail.com>
2023-09-21 17:01:10 +0600

    refactor

--- helper-api/src/test/java/com/kazatustudent/helper/api/service/FileDownloadServiceTest.java
+++ helper-api/src/test/java/com/kazatustudent/helper/api/service/FileDownloadServiceTest.java
@@ -7,14 +7,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.File;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.FileTime;
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 @ExtendWith(MockitoExtension.class)
 class FileDownloadServiceTest {
 
